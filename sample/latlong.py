891f7bcf07bbdfdfadd6eec3335bf7dbd73e34d8
bearmonkeycat <ckmiller88@gmail.com>
2020-02-22 22:34:13 -0800

    first data run complete plus 3d plotting

--- sample/latlong.py
+++
@@ -1,6 +0,0 @@
-from  geopy.geocoders import Nominatim
-geolocator = Nominatim()
-city ="London"
-country ="Uk"
-loc = geolocator.geocode(city+','+ country)
-print("latitude is :-" ,loc.latitude,"\nlongtitude is:-" ,loc.longitude)
