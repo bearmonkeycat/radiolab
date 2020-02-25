82efe117b1e39fbb60e5913ab256ba118df6de94
bearmonkeycat <ckmiller88@gmail.com>
2020-02-24 22:11:25 -0800

    cleaned up stuff

--- sample/filter.py
+++ sample/filter.py
@@ -11,22 +11,33 @@ lsoutput = subprocess.Popen('ls', stdout=subprocess.PIPE)
 filenames = files.decode('utf-8')
 filenames_list = filenames.split()
 
+# sample rate
+base_rate = 62.5e6
+divisor = 12
+sr = 1/(base_rate/divisor)
+
 # load data
 data = dict()
-bar = Bar('Processing Data', max=200)
-for i in range(200):
-    data[f"dataset_{i}"] = np.loadtxt(filenames_list[i])
+bar = Bar('Processing Data', max=100)
+for i in range(100):
+    data[f"dataset_{i}"] = [np.loadtxt(filenames_list[i])]
+    complex_data = np.empty(16000, dtype=complex)
+    complex_data.real = data[f"dataset_{i}"][0][:16000]
+    complex_data.imag = data[f"dataset_{i}"][0][16000:]
+    data[f"dataset_{i}"].append(complex_data)
+    data[f"dataset_{i}"].append(np.abs(np.fft.fft(data[f"dataset_{i}"][1]))**2)
+    data[f"dataset_{i}"].append(np.fft.fftfreq(n=len(data[f"dataset_{i}"][1]), d=sr))
     bar.next()
 bar.finish()
 
 # average data
 averages = []
-bar = Bar('Averaging Data', max=200)
+bar = Bar('Averaging Data', max=100)
 for data_set in data:
-    averages.append(data[data_set])
+    averages.append(data[data_set][2])
     bar.next()
 bar.finish()
 average = np.average(averages, axis=0)
 
 # write output
-np.savetxt('averaged_ouput', average)
+np.savetxt('avg-pspec-heart-off', average)
